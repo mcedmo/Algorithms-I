340131f5beb29113842692ad0e7400a964233b80
mcedmo <matthewcedmond@gmail.com>
2020-04-18 16:45:50 -0500

    cleaned up folders

--- week-2-queues/Deque.java
+++ week-2-queues/Deque.java
@@ -1,7 +1,11 @@
 public class Deque<Item> implements Iterable<Item> {
+    //Dequeue. A double-ended queue or deque (pronounced “deck”) is a generalization of a stack and a 
+    //queue that supports adding and removing items from either the front or the back of the data structure. 
 
     // construct an empty deque
-    public Deque()
+    public Deque() {
+        
+    }
 
     // is the deque empty?
     public boolean isEmpty()
