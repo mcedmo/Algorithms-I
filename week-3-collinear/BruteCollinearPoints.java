32abb78bfffdef71e8aa77ca9ca4d01ba7d53611
mcedmo <matthewcedmond@gmail.com>
2020-05-01 21:55:38 -0500

    worked on week 3

--- week-3-collinear/BruteCollinearPoints.java
+++ week-3-collinear/BruteCollinearPoints.java
@@ -1,17 +1,99 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BruteCollinearPoints {
+
+    private LineSegment[] segmentsTemp;
+
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
-        // check if the two points are equal 
-        for (int i = 0; i < points.length; i++) {
-            for (int x = i; x < points.length; x++) {
-                double slope = points[i].slopeTo(points[x]);
+        checkNull(points);
+        Arrays.sort(points);
+        checkDuplicates(points);
+        List<LineSegment> results = new ArrayList<LineSegment>();
+        int n = points.length;
 
-                for ()
+        // check if the two points are equal
+        for (int p = 0; p < n - 3; p++) {
+            for (int q = 1 + p; q < n - 2; q++) {
+                double targetSlope = points[p].slopeTo(points[q]);
+                for (int r = 1 + q; r < n - 1; r++) {
+                    if (points[q].slopeTo(points[r]) == targetSlope) {
+                        for (int s = 1 + r; s < n; s++) {
+                            if (points[q].slopeTo(points[s]) == targetSlope) {
+                                LineSegment newSegment = new LineSegment(points[p], points[s]);
+                                results.add(newSegment);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        segmentsTemp = results.toArray(new LineSegment[0]);
+
+    }
+
+    // check for duplicate points
+    private void checkDuplicates(Point[] points) {
+        for (int i = 0; i < points.length - 1; i++) {
+            if (points[i].compareTo(points[i + 1]) == 0) {
+                throw new IllegalArgumentException("duplicate points");
             }
         }
     }
 
-    public int numberOfSegments()        // the number of line segments
+    private void checkNull(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException("contains no points");
+        }
+        for (Point point : points) {
+            if (point == null) throw new IllegalArgumentException("Contains a null point");
+        }
 
-    public LineSegment[] segments()                // the line segments
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return segmentsTemp.length;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return segmentsTemp.clone();
+    }
+
+    // client provided by Princeton university
+    public static void main(String[] args) {
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
 }
--- week-3-collinear/FastCollinearPoints.java
+++ week-3-collinear/FastCollinearPoints.java
@@ -1,8 +1,31 @@
+import java.util.Arrays;
+
 public class FastCollinearPoints {
-    public FastCollinearPoints(
-            Point[] points)     // finds all line segments containing 4 or more points
 
-    public int numberOfSegments()        // the number of line segments
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+        Arrays.sort(points);
+        for (int y = 0; y < points.length; y++) {
+            Point p = points[y];
+            Point[] pointsBySlope = points.clone();
+            Arrays.sort(pointsBySlope, p.slopeOrder());
 
-    public LineSegment[] segments()                // the line segments
+            for (int i = 0; i < pointsBySlope.length - 2; i++) {
+                if (pointsBySlope[i] == pointsBySlope[i + 1] && pointsBySlope[i] == pointsBySlope[i
+                        + 2]) {
+                    for (int j = i + 3; j <)
+                }
+            }
+        }
+    }
+
+/*    // the number of line segments
+    public int numberOfSegments() {
+
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+
+    }*/
 }
